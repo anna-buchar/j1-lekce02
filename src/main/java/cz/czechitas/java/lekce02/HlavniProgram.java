b394a5c4ce6a2a648ade82ee3c98ce93fc619a93
Anna <anickab99@gmail.com>
2025-04-08 20:35:08 +0200

    zofka kresli prasatko, melo by fungovat s libovolnou delkou strany

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -13,6 +13,46 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO tady bude kód
+
+    int delkaStrany = 200;
+    int delkaNozicky = delkaStrany / 5;
+
+   zofka.turnRight(90);
+    nakreslitDomecek(delkaStrany);
+    zofka.turnLeft(45);
+    nakreslitDveNozicky(delkaNozicky);
+    zofka.turnLeft(135);
+    zofka.move(delkaStrany);
+    zofka.turnLeft(90);
+    nakreslitDveNozicky(delkaNozicky);
+
+
+  }
+
+  public void nakreslitDomecek(int delkaStrany) {
+    for (int i = 0; i < 4; i++) {
+      zofka.move(delkaStrany);
+      zofka.turnRight(90);
+    }
+    zofka.penUp();
+    zofka.move(delkaStrany);
+    zofka.penDown();
+    zofka.turnRight(45);
+    zofka.move(delkaStrany*(0.7));
+    zofka.turnRight(90);
+    zofka.move(delkaStrany*(0.7));
+  }
+
+  public void nakreslitDveNozicky(int delkaNozicky) {
+    zofka.turnLeft(45);
+    zofka.penDown();
+    zofka.move(delkaNozicky);
+    zofka.penUp();
+    zofka.turnRight(135);
+    zofka.move(delkaNozicky*(1.5));
+    zofka.turnRight(135);
+    zofka.penDown();
+    zofka.move(delkaNozicky);
   }
 
 }
